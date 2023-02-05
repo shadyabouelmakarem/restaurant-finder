2fc389051ce690534b67655e8e02d86c653a6c63
Shady Abouelmakarem <shadyabouelmakarem@gmail.com>
2023-02-05 23:56:29 +0100

    feat: add TS interfaces

---
+++ src/interfaces.ts
@@ -0,0 +1,27 @@
+export interface Restaurant {
+  name: string;
+  id: string;
+  url: string;
+  phone: string;
+  display_phone: string;
+  rating: number;
+  photos: string[];
+  location: Location;
+  reviews: Review[];
+}
+
+export interface Location {
+  address1: string;
+  city: City;
+  postal_code: string;
+  formatted_address: string;
+}
+
+export enum City {
+  Hamburg = "Hamburg",
+}
+
+export interface Review {
+  text: string;
+  rating: number;
+}
