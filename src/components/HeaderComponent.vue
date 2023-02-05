d57efe6c7a2345669115c6989e4a3666434fd1cf
Shady Abouelmakarem <shadyabouelmakarem@gmail.com>
2023-02-06 00:29:44 +0100

    feat: implemented header component

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>Restaurant finder</header>
+</template>
+
+<style lang="scss" scoped>
+header {
+  grid-area: a;
+  font-size: var(--font-size-huge);
+  font-weight: bold;
+  padding: 25px;
+  color: var(--white);
+  background: var(--dark-gray);
+}
+</style>
