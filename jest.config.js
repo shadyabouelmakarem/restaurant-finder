cfa2530da1b291ce3ebfb4be85f11af42d4bbc9f
Shady Abouelmakarem <shadyabouelmakarem@gmail.com>
2023-02-07 19:17:41 +0100

    test: configure jest

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,8 @@ module.exports = {
   preset: "@vue/cli-plugin-unit-jest/presets/no-babel",
   transform: {
     ".*\\.(vue)$": "@vue/vue3-jest",
+    "^.+\\.(ts|tsx)?$": "ts-jest",
+    "^.+\\.(js|jsx)$": "babel-jest",
   },
-  moduleFileExtensions: ["js", "jsx", "json", "vue"],
+  moduleFileExtensions: ["js", "jsx", "json", "vue", "ts"],
 };
